bfbd9a22f609ac24a6900cbe41c68e075c35e3a3
NagaVemprala <Naga.Vemprala@gmail.com>
2017-05-21 08:35:44 +0530

    Virtual Environment Script

---
+++ virtualenv.sh
@@ -0,0 +1,6 @@
+# If Anaconda is already installed use conda install virtualenv for setting up virtual environment 
+# Go to respective directory as cd Desktop/Twitter-Feeds-Fraud/
+cd /Users/nagavemprala/Desktop/Twitter-Feeds-Fraud
+virtualenv Fraud_Feeds
+source activate Fraud_Feeds
+# end the active session using -- source deactivate Fraud_Feeds 
